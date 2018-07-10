3cfd3fd6f7a8a50a2cfd704b1ce96e11c357e9f4
Jason Dong <jasondong03@icloud.com>
2018-07-10 10:24:35 -0500

    Set Up API Protocols

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class SourcesViewController: UITableViewController {
 
+    
+    var sources = [[String: String]]()
+    let apiKey = "5d892509a49046a087917c466fa80d09"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.title = "News Source"
+          let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
     }
 
+    func parse(json: JSON) {
+        for result in json["sources"].arrayValue{
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
