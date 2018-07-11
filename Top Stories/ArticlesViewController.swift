3bea77985ee5cbb7e847260802d8612fa62e51d2
Jason Dong <jasondong03@icloud.com>
2018-07-10 22:26:51 -0500

    Made more changes to api stuff

--- Top Stories/ArticlesViewController.swift
+++ Top Stories/ArticlesViewController.swift
@@ -9,38 +9,47 @@
 import UIKit
 
 class ArticlesViewController: UITableViewController {
-
-    var articles = [[String: String]]()
-    var apiKey = ""
-    var source = [String:String]()
+    
+    var episodes = [[String: String]]()
+    var nameKey = ""
+    var source = [String: String]()
+    var query : String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Top Stories"
-        let query = "https://newsapi.org/v1/articles?" + "source=\(source["id"]!)&apiKey=\(apiKey)"
-        DispatchQueue.main.async {
+        self.title = "Rick and Morty Episodes"
+        DispatchQueue.global(qos: .userInitiated) .async {
             [unowned self] in
-            if let url = URL(string: query) {
+            if let url = URL(string: self.query) {
                 if let data = try? Data(contentsOf: url) {
                     let json = try! JSON(data: data)
-                    if json["status"] == "ok" {
-                        self.parse(json: json)
-                        return
-                    }
+                    self.parse(json: json)
+                    return
                 }
             }
             self.loadError()
         }
     }
-    func parse(json: JSON) {
-        for result in json["articles"].arrayValue {
-            let title = result["title"].stringValue
-            let description = result["description"].stringValue
-            let url = result["url"].stringValue
-            let article = ["title": title, "description": description, "url": url]
-            articles.append(article)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let article = episodes[indexPath.row]
+        cell.textLabel?.text = article["name"]
+        cell.detailTextLabel?.text = article["url"]
+        return cell
+    }
+    
+    func parse(json: JSON){
+        for result in json["info"].arrayValue {
+            let airDate = result["air_date"].stringValue
+            let name = result["name"].stringValue
+            let imageURL = result["url"].stringValue
+            let source = ["air_date": airDate, "name": name, "url": imageURL]
+            episodes.append(source)
         }
-
         DispatchQueue.main.async {
             [unowned self] in
             self.tableView.reloadData()
@@ -50,24 +59,12 @@ class ArticlesViewController: UITableViewController {
     func loadError() {
         DispatchQueue.main.async {
             [unowned self] in
-            let alert = UIAlertController(title: "Loading Error",
-                                          message: "There was a problem loading the news feed",
+            let alert = UIAlertController(title: "Problem Loading",
+                                          message: "Can't load the page",
                                           preferredStyle: .actionSheet)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articles.count
-    }
-    
-    override func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let article = articles[indexPath.row]
-        cell.textLabel?.text = article["title"]
-        cell.detailTextLabel?.text = article["description"]
-        return cell
     }
     
     override func didReceiveMemoryWarning() {
@@ -75,9 +72,8 @@ class ArticlesViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let url = URL(string: articles[indexPath.row]["url"]!)
+        let url = URL(string: episodes[indexPath.row]["url"]!)
         UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
     }
-    
 }
 
